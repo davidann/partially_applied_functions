0f4b58e4b2b4fd7d1a324c6f566536d501149355
David Weiser <davidann@moz.com>
2013-10-14 21:33:02 -0700

    initial stab at creating a spec for a partially applied methods module

---
+++ spec/partially_applied_methods_spec.rb
@@ -0,0 +1,19 @@
+require 'lib/partially_applied_methods'
+require 'rspec'
+
+module Test
+  class A
+    extend PartiallyAppliedMethods
+
+    partially_applied_method :f do |a, b|
+      a + b
+    end
+  end
+end
+
+describe  Test::A::PartiallyAppliedMethods do
+  it "returns a methods if called with 1 of 2 arguments" do
+    described_class.f(1).should be_kind_of(Function)
+  end
+end
+
