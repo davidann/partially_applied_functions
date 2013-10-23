80de5ab5ad4b2be674f7e1edc9fd7d281dab5d4b
David Weiser <davidann@moz.com>
2013-10-22 20:19:29 -0700

    returns a proc if we only call a partially applied method.

--- lib/partially_applied_methods.rb
+++ lib/partially_applied_methods.rb
@@ -1,5 +1,19 @@
 module PartiallyAppliedMethods
   def partially_applied_method(method_name, &block)
+    arity = block.arity
+    define_singleton_method(method_name) do |*args|
+      _args = args
+      if _args.count == arity
+        block.call(*_args)
+      else
+        Proc.new do |*extra_args|
+          current_args = _args
+          current_args << extra_args
+          block.call(*current_args) if current_args == args.arity
+        end
+      end
+
+    end
 
   end
 end
--- spec/partially_applied_methods_spec.rb
+++ spec/partially_applied_methods_spec.rb
@@ -12,8 +12,14 @@ module Test
 end
 
 describe  Test::A do
-  it "returns a methods if called with 1 of 2 arguments" do
-    described_class.f(1).should be_kind_of(Function)
+  it "returns a method if called with 1 of 2 arguments" do
+    p_method = described_class.f(1)
+    p_method.should be_kind_of(Proc)
   end
+
+  it "runs computes the method reslt if given all the arguments at once" do
+    described_class.f(1,2).should == 3
+  end
+
 end
 
